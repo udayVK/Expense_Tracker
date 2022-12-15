823d868df8e7dfafa1711a4cf9d31632701b3640
Uday. V Krishna <vamshi8724@.gmail.com>
2022-12-15 20:19:39 +0530

    able to repeat a spend in front end

--- src/app/pojo/spend.ts
+++ src/app/pojo/spend.ts
@@ -3,10 +3,10 @@ import { Category, defaultCategory } from "./category";
 export interface Spend{
     id:number;
     purpose:string;
-    date:Date;
+    date:string;
     money:number;
     forOthers:number;
     category:Category;
 }
 
-export const defaultSpend = {id:NaN,purpose:'',date:new Date(),money:NaN,forOthers:NaN,category:defaultCategory} ;
+export const defaultSpend = {id:NaN,purpose:'',date:'',money:NaN,forOthers:NaN,category:defaultCategory} ;
